e020ddd3b3aa82049fa294d52b87918afd4978c9
Geraldo Atilio Bertoli <geraldoatiliobertoli@gmail.com>
2019-10-02 13:15:02 -0300

    Commit inicial

---
+++ src/main/java/com/geraldobertoli/buscadordearquivos/App.java
@@ -0,0 +1,13 @@
+package com.geraldobertoli.buscadordearquivos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Buscador de arquivos!" );
+    }
+}
